b37cc985334fd3450aa8d57429c7de12ab89fbf6
wknowpark797 <wknowpark797@gmail.com>
2023-06-16 10:19:59 +0900

    생성자 함수 내부에 클릭이벤트 추가

--- js/main.js
+++ js/main.js
@@ -22,24 +22,40 @@
 */
 function FontStyle(el, option) {
 	this.el = document.querySelector(el);
+	console.log('this: ', this);
 
-	if (!option) {
-		this.changeSize('30px');
-		this.changeColor('gray');
-		return;
-	}
+	this.el.addEventListener(
+		'click',
+		function () {
+			console.log('this: ', this);
 
-	// 무조건 실행되어야 하는 요소를 생성자 함수 내부에서 호출
-	if (option.type === 'all') {
-		this.changeSize(option.size);
-		this.changeColor(option.color);
-	}
-	if (option.type === 'size') {
-		this.changeSize(option.size);
-	}
-	if (option.type === 'color') {
-		this.changeColor(option.color);
-	}
+			if (!option) {
+				this.changeSize('30px');
+				this.changeColor('gray');
+				return;
+			}
+
+			// 무조건 실행되어야 하는 요소를 생성자 함수 내부에서 호출
+			if (option.type === 'all') {
+				this.changeSize(option.size);
+				this.changeColor(option.color);
+			}
+			if (option.type === 'size') {
+				this.changeSize(option.size);
+			}
+			if (option.type === 'color') {
+				this.changeColor(option.color);
+			}
+		}.bind(this)
+	);
+	/*
+    일반 이벤트가 연결되어 있는 function 문 안쪽에서 this 값이 이벤트가 발생한 선택자로 변경되기 때문에 해당 값을 해당 function 문 외부에서 bind(this)로 고정
+
+    [ 일반 함수에서 이벤트 사용 시 이슈 발생 ]
+      이벤트 안쪽에서 this는 클릭 요소를 가리킨다.
+      따라서 프로토타입 함수를 불러오지 못한다.
+      -> bind 사용
+  */
 }
 
 FontStyle.prototype.changeSize = function (size) {
