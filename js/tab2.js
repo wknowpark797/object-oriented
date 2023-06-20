618e3e0a022008ef2f551bbde337f84b14a42dd9
wknowpark797 <wknowpark797@gmail.com>
2023-06-20 12:24:53 +0900

    객체지향 탭 만들기 코드 개선 (ES6)

--- js/tab2.js
+++ js/tab2.js
@@ -1,9 +1,17 @@
 // ES6
 class Tab {
 	constructor(selector, option) {
+		// 지역변수 설정 이유
+		// defaults, result 값은 인스턴스 객체에 전달할 필요가 없어 지역변수 설정
+		// this 객체에 특정 정보값을 복사하여 전달하는 구조이기 때문에 this로 복사해야 하는 정보값을 줄이는것을 권장
+		// defaults, result 값은 btns와 boxs를 만들어주기 위한 값으로 인스턴스 객체에서 직접적으로 필요한 값이 아니기 때문에 지역변수로 설정
+		// 생성자를 통해서 복사가 될 때마다 지연변수로 설정한 값은 static 하게 유지
+		const defaults = { btns: 'ul li', boxs: 'article div' };
+		const result = { ...defaults, ...option };
+
 		this.tab = document.querySelector(selector);
-		this.btns = this.tab.querySelectorAll(option.btns);
-		this.boxs = this.tab.querySelectorAll(option.boxs);
+		this.btns = this.tab.querySelectorAll(result.btns);
+		this.boxs = this.tab.querySelectorAll(result.boxs);
 		this.bindingEvent();
 	}
 
@@ -20,8 +28,3 @@ class Tab {
 		arr[idx].classList.add('on');
 	}
 }
-
-new Tab('#tab', {
-	btns: 'ul li',
-	boxs: 'article div',
-});
