84e79a848197fdc81545e72faed0aa8fe5febacc
unknown <wknowpark797@gmail.com>
2023-08-14 21:42:21 +0900

    tab 예제를 통한 객체지향 캡슐화 테스트 복습 및 주석정리

--- js/tab2.js
+++ js/tab2.js
@@ -2,20 +2,22 @@
 
 /*
 	[ 캡슐화 ]
-	
-	property 접근자
-	- get: 프로퍼티의 key에 접근할 때 자동으로 호출되는 접근자
-	- set: 프로퍼티의 value에 값이 담길 때 자동으로 호출되는 접근자
+	- property 접근자
+		- get: 프로퍼티의 key에 접근할 때 자동으로 호출되는 접근자
+		- set: 프로퍼티의 value에 값이 담길 때 자동으로 호출되는 접근자
 
-	클래스에 직접 값을 등록할 때
-	- public: 클래스에 등록한 값을 접근해서 변경
-	- static: 고정적인 상태로 값을 저장 (접근가능, 확인가능, 변경가능)
-	- private: 접근 및 변경 불가능 (#키워드 사용)
+	- 클래스에 직접 값을 등록할 때
+		- public: 클래스에 등록한 값을 접근해서 변경
+		- static: 고정적인 상태로 값을 저장 (접근가능, 확인가능, 변경가능)
+		- private: 접근 및 변경 불가능 (#키워드 사용)
 */
 
 class Tab {
-	// 인스턴스가 복사가 될 때마다 매번 메모리 할당을 할 필요가 없는 공통의 값을 클래스자체에 등록 처리
-	// 클래스에 등록한 변수 앞쪽에 #을 붙이면 private 설정으로 변경되고 private로 등록된 변수는 외부에서 변경 불가능한 캡슐화 상태
+	/*
+		- 인스턴스가 복사가 될 때마다 매번 메모리 할당을 할 필요가 없는 공통의 값을 클래스자체에 등록 처리
+		- 클래스에 등록한 변수 앞쪽에 #을 붙이면 private 설정으로 변경되고 
+			private로 등록된 변수는 외부에서 변경 불가능한 캡슐화 상태가 된다.
+	*/
 	#defaults = { btns: 'ul li', boxs: 'article div' };
 
 	constructor(selector, option) {
@@ -29,7 +31,7 @@ class Tab {
 			-> 인스턴스 객체에서 직접적으로 필요한 값이 아니기 때문에 지역변수로 설정
 		*/
 
-		const result = { ...this.defaults, ...option };
+		const result = { ...this.#defaults, ...option };
 
 		this.tab = document.querySelector(selector);
 		this.btns = this.tab.querySelectorAll(result.btns);
@@ -41,12 +43,12 @@ class Tab {
 		// this.bindingEvent();
 	}
 
-	// tab이라는 property에 key에 접근하는 순간 호출되는 함수
+	// tab 프로퍼티 key에 접근하는 순간 호출되는 함수
 	get tab() {
 		return this.value;
 	}
 
-	// tab이라는 property value에 값을 담으려는 순간 호출되는 함수
+	// tab 프로퍼티 value에 값을 담으려는 순간 호출되는 함수
 	set tab(value) {
 		this.value = value.tagName === 'SECTION' ? value : document.querySelector('#tab');
 	}
